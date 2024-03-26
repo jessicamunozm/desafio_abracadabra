0b9ce258265b4dee86997af1b38e199b1daf2440
Jessica Munoz <jessica.munozm@mail.udp.cl>
2024-03-25 23:30:41 -0300

    se desarrolla función random y se testea, funcionando ok

--- index.js
+++ index.js
@@ -20,36 +20,41 @@ app.get ('/abracadabra/usuarios', (req, res) => {
 
 // 4. Crear un middleware con la ruta /abracadabra/juego/:usuario para validar que el 
 // usuario recibido como parámetro “usuario” existe en el arreglo de nombres creado en 
-// el servidor. // si no es exitoso, devolver la imagen “who.jpeg”.
+// el servidor. // si es exitoso, devolver game.html, sino devolver no_user.html”.
 // Ruta para el juego
-app.get('/abracadabra/juego/:usuario?', (req, res) => {
-    const nameUser = req.params.usuario
-    if (!nameUser) {
-        res.redirect('/no_user.html')
-    } else {
-        const usuarioRequerido = usuario.includes(nameUser);
-        usuarioRequerido
-            ? res.sendFile(`${__dirname}/public/game.html`)
-            : res.redirect('/no_user.html')
-    }
+app.use('/abracadabra/juego/:usuario', (req, res, next) => {
+    const nameUser = req.params.usuario;
+    const usuarioRequerido = usuario.includes(nameUser);
+    usuarioRequerido
+        ? res.redirect('/abracadabra/game')
+        : res.redirect('/abracadabra/no_user');
+});
+
+app.get('/abracadabra/game', (req, res) => { 
+    res.sendFile (__dirname + "/public/game.html")
+})
+
+app.get('/abracadabra/no_user', (req, res) => { 
+    res.sendFile (__dirname + "/public/no_user.html")
 })
 
 // Crear una ruta /abracadabra/conejo/:n que valide si el parámetro “n” coincide con el 
 // número generado de forma aleatoria. En caso de ser exitoso, devolver la imagen del conejo.
-app.get('/abracadabra/rabbit', (req, res) => {
-    res.sendFile(__dirname + "/public/rabbit.html")
-});
-
 // de lo contrario devolver la imagen de Voldemort.
-app.get('/abracadabra/not_found', (req, res) => {
-    res.sendFile(__dirname + "/public/not_found.html")
-})
+app.get('/abracadabra/conejo/:n', (req, res) => {
+    const num = Math.floor(Math.random() * 4) + 1
+    const n = parseInt(req.params.n)
+    if (n === num) {
+        res.sendFile(__dirname + "/public/rabbit.html")
+    } else {
+        res.sendFile(__dirname + '/public/not_found.html')
+    }
+});
 
 // 6. Crear una ruta genérica que devuelva un mensaje diciendo “Esta página no existe...” al 
 // consultar una ruta que no esté definida en el servidor. (
 app.get ('*', (req, res) => 
     res.send ('¡Abracadabra! El mago hizo desaparecer esta página'))
-
 
 app.listen (port, () => {
     console.log (`El servidor está inicializado en el puerto http://localhost:${port}`)
